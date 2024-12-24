9ce8bca365d3e4a0ff76d98e535002acde347644
Athaya Razan <142677331+Numenor08@users.noreply.github.com>
2024-12-24 12:18:19 +0700

    FIx manage user table

--- pergudangan-frontend/src/components/Columns.jsx
+++ pergudangan-frontend/src/components/Columns.jsx
@@ -472,7 +472,7 @@ export const userColumns = [
                 variant="ghost"
                 onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
             >
-                Email
+                Role
                 <ArrowUpDown className="ml-2 h-4 w-4" />
             </Button>
         ),
